6d5961e1d46197d5631b190575e0a2ba12026925
prumor <prumor@inventa.com>
2019-01-31 17:50:55 +0000

    - 1st

---
+++ ghost.go
@@ -0,0 +1,59 @@
+package ghost
+
+import (
+	"github.com/briandowns/spinner"
+	"github.com/fatih/color"
+	"time"
+)
+
+var console = color.New(color.FgRed, color.Bold)
+var spi = spinner.New(spinner.CharSets[11], 100*time.Millisecond)
+
+type Ghost struct {
+	Config map[string]interface{}
+	Rest   *Rest
+}
+
+type Rest struct {
+	Proxy            interface{}
+	Auth             interface{}
+	Payload          interface{}
+	Response         string
+	Type             string
+	ContentType      string
+	RetryCount       int
+	RetryWaitTime    time.Duration
+	RetryMaxWaitTime time.Duration
+	URL              string
+	QueryParams      map[string]string
+}
+
+type Proxy struct {
+	Ip string
+}
+
+type ProxyGateway struct {
+	Ip     []string
+	Random bool
+}
+
+type Token struct {
+	Key   string
+	Value string
+}
+
+type BasicAuth struct {
+	Username string
+	Password string
+}
+
+type DataModel struct {
+	Data interface{}
+}
+
+/*func (g Ghost) New(auth interface{}, contentType string) *Ghost {
+	g.Rest.Auth = auth
+	g.Rest.ContentType = contentType
+	return &g
+}
+*/
---
+++ request.go
@@ -0,0 +1,87 @@
+package ghost
+
+import (
+	"encoding/json"
+	"github.com/go-resty/resty"
+	"math/rand"
+	"time"
+)
+
+type RestResponse struct {
+	Raw 		*resty.Response
+	String 		string
+}
+
+var Unmarshal = func(payload string, v interface{}) error {
+	return json.Unmarshal([]byte(payload), &v)
+}
+
+func (g Ghost) MakeRequest() (r RestResponse, err error) {
+	return g.request()
+}
+
+func (g Ghost) request() (r RestResponse, err error){
+
+	resp := resty.SetRetryCount(g.Rest.RetryCount).
+		SetRetryWaitTime(g.Rest.RetryWaitTime).
+		SetRetryMaxWaitTime(g.Rest.RetryMaxWaitTime)
+
+	if g.Rest.ContentType != "" {
+		resp.SetHeader("Content-Type", g.Rest.ContentType)
+	}
+
+	if g.Rest.Proxy != nil {
+		switch v := g.Rest.Proxy.(type) {
+		case *Proxy:
+			resp.SetProxy(g.Rest.Proxy.(*Proxy).Ip)
+		case *ProxyGateway:
+			rand.Seed(time.Now().Unix())
+			n := rand.Int() % len(g.Rest.Proxy.(*ProxyGateway).Ip)
+			if g.Rest.Proxy.(*ProxyGateway).Random {
+				resp.SetProxy(g.Rest.Proxy.(*ProxyGateway).Ip[n])
+			}
+		default:
+			_, err := console.Println("[✖] Unexpected Auth type, why?", v)
+			return r, err
+		}
+	}
+
+	switch v := g.Rest.Auth.(type) {
+	case *Token:
+		resp.SetHeader(g.Rest.Auth.(*Token).Value, g.Rest.Auth.(*Token).Value)
+	case *BasicAuth:
+		resp.SetBasicAuth(g.Rest.Auth.(*BasicAuth).Username, g.Rest.Auth.(*BasicAuth).Password)
+	default:
+		_, err := console.Println("[✖] Unexpected Auth type, why?", v)
+		return r, err
+	}
+
+	if g.Rest.QueryParams != nil {
+		resp.SetQueryParams(g.Rest.QueryParams)
+	}
+
+	if g.Rest.Type == "GET" {
+		resp, err := resp.R().Get(g.Rest.URL)
+		if err != nil {
+			_, err := console.Println("[✖] Unexpected GET request, why?", err)
+			return r, err
+		}
+
+		r.Raw = resp
+		r.String = resp.String()
+		return r, err
+
+	} else {
+		resp, err := resp.R().SetBody(g.Rest.Payload).Post(g.Rest.URL)
+		if err != nil {
+			_, err := console.Printf("[✖] Unexpected POST request, why?", err)
+			return r, err
+		}
+
+		r.Raw = resp
+		r.String = resp.String()
+		return r, err
+
+	}
+
+}
